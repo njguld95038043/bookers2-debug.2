c8776fb87c384ba3bcc52fb36894dc069185500b
njguld95038043 <t.tamura6060@gmail.com>
2022-02-07 08:40:32 +0000

    [Finish]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,7 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range = "User"
+    if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
